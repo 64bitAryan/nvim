98f930fab82476d6f403cadb83fc6331e7712a46
Aryan Patel <patelaryan1726@gmail.com>
2024-02-07 13:25:16 +0530

    added cmp

--- lua/theprimeagen/lazy/nvim-cmp.lua
+++ lua/theprimeagen/lazy/nvim-cmp.lua
@@ -8,8 +8,10 @@ return {
         'hrsh7th/cmp-cmdline',
         'L3MON4D3/LuaSnip',
         "saadparwaiz1/cmp_luasnip",
+        "j-hui/fidget.nvim",
     },
     config = function()
+        require("fidget").setup()
         local cmp = require("cmp")
         local luasnip = require("luasnip")
         local cmp_select = {behavior = cmp.SelectBehavior.Select}
